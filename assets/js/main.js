bca1c4a4df247078bdbc99de51206deea2a59009
eliter daneo <eliterdaneo13@gmail.com>
2024-11-07 18:35:22 +0700

    fix beberapa bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,24 @@
-const endpoint = "http://10.10.10.01";
+const endpoint = "http://10.10.10.01"; // Alamat endpoint API yang akan diakses untuk mengatur dan mengambil status LED
 
+// Fungsi untuk mendapatkan status LED di ruang tamu
 function getTamuLed() {
   fetch(endpoint + "/tamu", {
     method: "GET",
   })
-    .then((response) => response.text())
+    .then((response) => response.text()) // Ambil teks respons dari server
     .then((result) => {
       if (result == "ON") {
-        ledTamu.style.background = "blue";
-        tamuLedImage.src = "./assets/img/led-on.png";
+        // Jika LED menyala
+        ledTamu.style.background = "blue"; // Ubah latar belakang elemen LED tamu menjadi biru
+        tamuLedImage.src = "./assets/img/led-on.png"; // Tampilkan gambar LED menyala
       } else {
-        ledTamu.style.background = "#579fff";
-        tamuLedImage.src = "./assets/img/led-off.png";
+        ledTamu.style.background = "#579fff"; // Latar belakang LED tamu menjadi warna default
+        tamuLedImage.src = "./assets/img/led-off.png"; // Tampilkan gambar LED mati
       }
     });
 }
 
+// Fungsi untuk mendapatkan status LED di ruang lab
 function getLabLed() {
   fetch(endpoint + "/lab", {
     method: "GET",
@@ -32,6 +35,7 @@ function getLabLed() {
     });
 }
 
+// Fungsi untuk mendapatkan status LED di kamar mandi (WC)
 function getWcLed() {
   fetch(endpoint + "/wc", {
     method: "GET",
@@ -48,6 +52,7 @@ function getWcLed() {
     });
 }
 
+// Fungsi untuk mendapatkan status LED di dapur
 function getDapurLed() {
   fetch(endpoint + "/dapur", {
     method: "GET",
@@ -64,24 +69,27 @@ function getDapurLed() {
     });
 }
 
+// Fungsi untuk mengubah status LED di ruang tamu
 function setTamuLed() {
   fetch(endpoint + "/tamu", {
     method: "POST",
   })
-    .then((response) => response.text())
-    .then(() => location.reload());
+    .then((response) => response.text()) // Ambil respons teks dari server
+    .then(() => location.reload()); // Refresh halaman untuk memperbarui status LED
 }
 
+// Fungsi untuk mengubah status LED di ruang lab
 function setLabLed() {
   fetch(endpoint + "/lab", {
     method: "POST",
   })
     .then((response) => response.text())
     .then(() => location.reload());
-  ledLab.style.background = "red";
-  labLedImage.src = "./assets/img/led-on.png";
+  ledLab.style.background = "red"; // Ubah warna latar LED lab saat diubah
+  labLedImage.src = "./assets/img/led-on.png"; // Tampilkan gambar LED menyala
 }
 
+// Fungsi untuk mengubah status LED di kamar mandi (WC)
 function setWcLed() {
   fetch(endpoint + "/wc", {
     method: "POST",
@@ -92,6 +100,7 @@ function setWcLed() {
   wcLedImage.src = "./assets/img/led-on.png";
 }
 
+// Fungsi untuk mengubah status LED di dapur
 function setDapurLed() {
   fetch(endpoint + "/dapur", {
     method: "POST",
@@ -102,6 +111,7 @@ function setDapurLed() {
   dapurLedImage.src = "./assets/img/led-on.png";
 }
 
+// Memanggil fungsi untuk mendapatkan status awal setiap LED saat halaman pertama kali dimuat
 getTamuLed();
 getLabLed();
 getWcLed();
